87cc026bf5fd071ce21ccdffab0752ee33d3a977
Noah <113019657+mhmd-noah@users.noreply.github.com>
2023-01-09 18:42:44 +0100

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,15 @@
+function fun_open_qr() {
+  y = document.getElementById("qr_message").style.visibility = "visible";
+  if (y == "visible") {
+    x = document.getElementById("not_found").style.filter = "blur(5px)";
+    if (x == "blur(5px)") {
+      document.getElementById("qr_message").style.filter = "none(0px)";
+    }
+  }
+}
+function fun_close_qr() {
+  document.getElementById("qr_message").style.visibility = "hidden";
+  if (document.getElementById("qr_message").style.visibility == "hidden") {
+    document.getElementById("not_found").style.filter = " blur(0px)";
+  }
+}
